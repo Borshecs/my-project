fb27d0e758307038f9257db5ad22891aa613f143
Alexey <37597550+Borshecs@users.noreply.github.com>
2023-05-09 04:12:50 +0300

    pipe

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="@/assets/logo.png" alt="#" width="60" height="60"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="(menu, index) in headerMenuList" :key="index">
+            <router-link class="nav-link active" aria-current="page" :to="menu.path">
+              {{ menu.name }}
+            </router-link>
+          </li>
+        </ul>
+       <div>
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            
+            <li class="nav-item" v-for="(menu, index) in headerMenuList2" :key="index">
+              <router-link class="nav-link active" aria-current="page" :to="menu.path">
+                {{ menu.name }}
+              </router-link>
+              
+            </li>
+          </ul>
+        </div>
+       </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      headerMenuList: [
+        { name: 'Review', path: './HomeView' },
+        { name: 'Possibilities', path: './possibilities' },
+        { name: 'Plot', path: './aboutSite' },
+        { name: 'News', path: './news' },
+      ],
+      headerMenuList2: [
+        { name: 'Sign', path: './sign' },
+        { name: 'Buy', path: './buy' },
+      ]
+    }
+  },
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+
+</style>
